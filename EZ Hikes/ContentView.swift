1e68988095c488dcbadc0d1faec313250083291a
Prayash Thapa <prayasht@gmail.com>
2019-10-03 12:43:44 -0600

    Initial Commit

---
+++ EZ Hikes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EZ Hikes
+//
+//  Created by Prayash Thapa on 10/3/19.
+//  Copyright © 2019 Viget. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
